0fcb464282184b487f4edfcc9dc6e2fd121beade
nico <nicogigena99@gmail.com>
2019-06-14 15:35:43 -0300

    2

--- js/home.js
+++ js/home.js
@@ -1,6 +1,5 @@
 window.onload = function(){
     //APIKey = 515c73c060475afdc6d4bfe35f81b7e3
-    console.log(1);
   var queryGeneros = document.querySelector("section.generos")
   var queryGenBtn = document.querySelector("div.gen-btn")
   var queryContainer = document.querySelector(".container");
@@ -16,6 +15,55 @@ window.onload = function(){
       document.querySelector(".close").classList.toggle("display-block")
     })
 
+//QUEDA SOLUCIONAR EL PRIMER TITULO
+  fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=515c73c060475afdc6d4bfe35f81b7e3&language=es-AR&page=1&region=US')
+      .then(function(response) {
+          return response.json();
+      })
+      .then(function(data) {
+          console.log('data = ', data);
+          var results = data.results
+          console.log(results);
+
+          for (var i = 0; i < 10; i++) {
+            var proxUl = document.querySelector("section.proximas div.lista-banner ul")
+            if (i==0) {
+              proxUl.innerHTML = "<li><button type='button' name='button' class='active' data-poster=" + results[i].poster_path + ">" + results[i].title + "</button></li>"
+            }else{
+              proxUl.innerHTML += "<li><button type='button' name='button' class='inactive' data-poster=" + results[i].poster_path + ">" + results[i].title + "</button></li>"
+            }
+          }//lista
+
+          var proxInactive=document.querySelectorAll("section.proximas div.lista-banner button.inactive")
+          var proxActive=document.querySelector("section.proximas div.lista-banner button.active")
+
+          document.querySelector("section.proximas div.banner").innerHTML = "<img src=https://image.tmdb.org/t/p/original/" + results[0].poster_path + ">"
+
+            for (var i = 0; i < proxInactive.length; i++) {
+              proxInactive[i].addEventListener("click",function (){
+                proxActive.classList.toggle("inactive");
+                proxActive.classList.remove("active");
+                var poster = this.getAttribute("data-poster");
+                document.querySelector("section.proximas div.banner img").setAttribute("src", "https://image.tmdb.org/t/p/original/" + poster);
+              })
+            }
+      })
+      .catch(function(err) {
+          console.error(err);
+      });
+
+
+
+//Me borra los btn
+    var valoInactive=document.querySelectorAll("section.valoradas div.lista-banner button.inactive")
+    var valoActive=document.querySelector("section.valoradas div.lista-banner button.active")
+    console.log(valoActive);
+    for (var i = 0; i < valoInactive.length; i++) {
+      valoInactive[i].addEventListener("click",function (){
+        valoActive.classList.toggle("inactive");
+        valoActive.classList.toggle("active");
+      })
+    }
 
 
 
@@ -45,11 +93,11 @@ window.onload = function(){
           return response.json();
       })
       .then(function(data) {
-          console.log('data = ', data);
+          // console.log('data = ', data);
           var generos = data.genres
-          console.log(generos);
+          // console.log(generos);
           for (var i = 0; i < generos.length; i++) {
-            console.log(generos[i].name);
+            // console.log(generos[i].name);
             document.querySelector(".generos ul").innerHTML += "<li><button type='button' name='button' class='btn w-100'>" + generos[i].name + "</button></li>"
           }
       })
@@ -62,9 +110,9 @@ window.onload = function(){
               return response.json();
           })
           .then(function(data) {
-              console.log('data = ', data);
+              // console.log('data = ', data);
               var pelis = data.results
-              console.log(pelis);
+              // console.log(pelis);
               for (var i = 0; i < 5; i++) {
                 if (i==0) {
                   document.querySelector("ol.carousel-indicators").innerHTML = "<li data-target='#myCarousel' data-slide-to='0' class='active'></li>"
